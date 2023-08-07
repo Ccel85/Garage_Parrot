1e4c2cdf10181f7af41bd78f6812dc0961916315
Ccel85 <ccelen90@gmail.com>
2023-08-07 16:45:07 +0200

    1er GIT

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/svg+xml" href="assets/logo/logo.svg">
+    <title>Garage V.Parrot</title>
+</head>
+<body>
+<div class="container">
+  <div class="row">
+<header>
+    <img src="assets/logo/logo.svg" alt="Logo Garage V.Parrot">
+    <div class="connection">
+    <img src="assets/logo/connection.svg">
+    <a   href="connection.php">Connection</a>
+    </div>
+    <nav class="nav justify-content-center border-bottom pb-3 mb-3">
+      <a href="accueil.php">Accueil</a>
+      <a href="nos_services.php">Nos services</a>
+      <a href="nos_occasions.php">Nos Occasions</a>
+      <a href="contact.php">Contact</a>
+</header>
+</div>
+</div>
+</body>
+</html>
