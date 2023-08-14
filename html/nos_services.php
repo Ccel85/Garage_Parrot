881fd64e45d0b3426e8b3e6cfbe473ac3e59191c
Ccel85 <ccelen90@gmail.com>
2023-08-14 18:05:43 +0200

    footer finalisé

---
+++ html/nos_services.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Garage V.Parrot</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js">
+<link rel="stylesheet" href="../css/style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php include'header.html';?>
+<section >
+    <div class="mecanique">
+        <img src="../assets/img/img_service_mecanique.png" alt="image mecanique">
+        <div class="texteMecanique">
+        <h2>MECANIQUE</h2>
+        <p>Nous intervenons sur tous types de travaux, sur de l’entretien classique aussi bien que sur de la mécanique lourde.<br>
+            Le garage effectue vos révisons périodiques et vidanges sur tous types de véhicules,même les plus récents.</p>
+        </div>
+    </div>
+</section>
+<section >
+    <div class="carrosserie">        
+        <div class="texteCarrosserie">
+        <h2>CARROSSERIE</h2>
+        <p>Nous disposons de notre propre atelier de carosserie et de sa cabine de peinture.<br>
+            Nous proposons du:</p>
+            <ul>
+            <li>-Redressage et remise en état</li>
+            <li>-Débosselage</li>
+            <li>-Peinture de votre carosserie</li>
+            <li>-Lustrage</li>
+            <li>-Vernissage</li>
+            </ul>
+        </div>
+        <img src="../assets/img/img_service_carrosserie.png" alt="image carrosserie">
+    </div>
+</section>
+
+</body>
+<?php include 'footer.html' ?>
