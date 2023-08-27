2357432a8af217a87eebe5c85206bcb49925e3e0
Ccel85 <ccelen90@gmail.com>
2023-08-27 11:56:55 +0200

    mise en page service

--- html/service.php
+++ html/service.php
@@ -1,9 +1,10 @@
+
 <div class="nosServices">
     <div class="service">
         <img  src='../assets//img/img_service_carrosserie.png' alt="image service">
         <div class="firstService">
             <h2><?php echo $service['title'] ?></h2>
-            <p><?php echo $service['description'] ?></p>
+            <p><?php echo nl2br(htmlspecialchars($service['description'])) ?></p>
         </div>
     </div>
 </div>
