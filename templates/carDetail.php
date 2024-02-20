e48be411701cf9b3866d0a71d1b90030f288d69d
Ccel 85 <ccelena90@gmail.com>
2024-02-20 23:46:44 +0100

    mise a jour des images dans cardetail

--- templates/carDetail.php
+++ templates/carDetail.php
@@ -6,156 +6,147 @@ include('../templates/header.php');?>
 
 <?php 
 if (isset($_GET['id'])) {
-  $carId = $_GET['id'];
-  // Utiliser l'ID pour récupérer les détails de l'annonce depuis la base de données
-  $carDetails = getCarById($adminpdo,$carId);}
-  // Vérifier si l'annonce existe
-  if ($carDetails) {?>
-
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb p-3 bg-body-tertiary rounded-3">
-        <li class="breadcrumb-item"><a href="../templates/index.php">Accueil </a></li>
-        <li class="breadcrumb-item"><a href="../templates/nos_occasions.php">Nos occasions </a></li>
-        <li class="breadcrumb-item active" aria-current="page"></li>
-</nav>
-<div class="container  announcement-container announcement-<?= $carDetails['id']?>" >  
-    <div id="myCarousel" class="carousel slide mb-6 pointer-event" data-bs-ride="carousel">
+    $carId = $_GET['id'];
+    // Utiliser l'ID pour récupérer les détails de l'annonce depuis la base de données
+    $carDetails = getCarById($adminpdo,$carId);
+    // Vérifier si l'annonce existe
+    if ($carDetails) {
+        // Récupérer les images de l'ID
+        $sql = "SELECT img1, img2, img3, img4 FROM cars WHERE id = :id";
+        $statement = $adminpdo->prepare($sql);  
+        $statement->bindParam(':id', $carId);
+        $statement->execute();
+        $images = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb p-3 bg-body-tertiary rounded-3">
+                <li class="breadcrumb-item"><a href="../templates/index.php">Accueil </a></li>
+                <li class="breadcrumb-item"><a href="../templates/nos_occasions.php">Nos occasions </a></li>
+                <li class="breadcrumb-item active" aria-current="page"></li>
+            </ol>
+        </nav>
+        <div class="container  announcement-container announcement-<?= $carDetails['id'] ?>" >  
+            <!-- Carousel -->
+            <div id="myCarousel" class="carousel slide mb-6 pointer-event" data-bs-ride="carousel">
+                <div class="carousel-inner text-center">
 <?php
-$idVehicule = $carId;
-// Chemin vers le dossier d'images du véhicule
-$cheminDossierImagesVehicule = '../assets/img/';
-// Liste des fichiers d'images dans le dossier du véhicule
-$fichiersImagesVehicule = scandir($cheminDossierImagesVehicule);
-// Filtrer les résultats pour ne conserver que les dossiers (en excluant . et ..)
-$dossiersImages = array_filter($fichiersImagesVehicule, function ($element) use ($cheminDossierImagesVehicule) {
-    return is_dir($cheminDossierImagesVehicule . '/' . $element) && !in_array($element, ['.', '..']);
-});
-$idVehicule  =$_GET['id'] + 1;
-// Utilisez cet index pour récupérer le nom du dossier image correspondant
-if (!empty($dossiersImages)) {
-  $nomDossierImage = $dossiersImages[$idVehicule];
-  $cheminDossier = $cheminDossierImagesVehicule . '/' . $nomDossierImage;
-  $contenuDossier = scandir($cheminDossier);
-// Filtrer les résultats pour ne conserver que les fichiers (en excluant . et ..)
-    $fichiersImages = array_filter($contenuDossier, function ($element) use ($cheminDossier) {
-    return is_file($cheminDossier . '/' . $element) && !in_array($element, ['.', '..']);
-      });
-      }
-// Afficher le carrousel seulement s'il y a des images dans ce dossier
-  if (!empty($fichiersImages)) {
-      echo '<div class="carrousel">';
-      echo '<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">';
-      echo '<div class="carousel-inner text-center ">';
-      foreach ($fichiersImages as $index => $fichierImage) {
-        echo '<div class="carousel-item imgcarousel' . ($index === 2 ? ' active' : '') . '">';
-        echo '<img class="card-img imgcarousel" src="' . $cheminDossier . '/' . $fichierImage . '" alt="' . $fichierImage . '">';
-        echo '</div>';
-      }
-      echo '</div>';
-// Boutons de contrôle pour passer à l'image suivante ou précédente
-    echo '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">';
-    echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
-    echo '<span class="visually-hidden">Previous</span>';
-    echo '</button>';
-    echo '<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">';
-    echo '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
-    echo '<span class="visually-hidden">Next</span>';
-    echo '</button>';
-    echo '</div>';
-    echo '</div>'; 
-    } else {
-        echo "Aucune image trouvée dans le dossier $dossier.";
-    }
-} else {
-    echo "Aucun dossier d'images trouvé.";
-}
-// Supposons que $carId contient l'ID du véhicule
-  $idVehicule  =$_GET['id'] + 1;
-  // Utilisez cet index pour récupérer le nom du dossier image correspondant
-  $nomDossierImage = $dossiersImages[$idVehicule];?>
-  <div>
-  <h1 class="carTitle"><?=$carDetails['modele']; ?></h1>
-    <br>
-    <h1 class="carContent"><?= $carDetails['carContent']; ?></h1>  
-  </div>
-  <div class="logo-card">
-    <div class="logos">
-      <img src="../assets/logo/logo_carburant.svg">
-      <p class=""><?= $carDetails['energy'];?></p>
+        // Parcourir le tableau d'images
+        foreach ($images as $index => $image) {
+            $img1 = $image['img1'];
+            $img2 = $image['img2'];
+            $img3 = $image['img3'];
+            $img4 = $image['img4'];
+?>
+                    <div class="carousel-item <?= $index === 0 ? 'active' : '' ?>">
+                        <img class="card-img imgcarousel" src="<?= $img1 ?>" alt="Image 1">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="card-img imgcarousel" src="<?= $img2 ?>" alt="Image 2">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="card-img imgcarousel" src="<?= $img3 ?>" alt="Image 3">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="card-img imgcarousel" src="<?= $img4 ?>" alt="Image 3">
+                    </div>
+<?php
+        }
+?>
+        
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="logos">
-      <img src="../assets/logo/logo_compteur_vitesse.svg">
-      <p class=""><?= $carDetails['km']; ?></p>
-    </div>
-    <div class="logos">
-      <img src="../assets/logo/logo_calendrier.svg">
-      <p class=""><?= $carDetails['year']; ?></p>
-    </div>
-  </div>
-    <hr>
-  <div class="carCaracteristique">
-    <div class="carGeneral">
-      <ul>  
-        <div class="carDetail">
-          <h2>Caracteristiques générales :</h2>
-          <li>                
-            <span class="titleDescritpion">Couleur :</span>
-            <span class="color"><?= $carDetails['color']; ?></span>
-          </li>
-          <li>
-            <span class="titleDescritpion">Boite :</span>
-            <span class="carBoite"><?= $carDetails['carBoite']; ?></span>
-          </li>
-          <li>
-            <span class="titleDescritpion">Nbre de porte :</span>
-            <span class="carDoor"><?= $carDetails['carDoor']; ?></span>
-          </li>
-          <li>
-            <span class="titleDescritpion">PuissanceFiscale :</span>
-            <span class="puissanceFiscale"><?= $carDetails['puissanceFiscale'].' CV'; ?></span>
-          </li>
-          <li>
-            <span class="titleDescritpion">Puissance :(DIN)</span>
-            <span class="Puissance"><?= $carDetails['Puissance'].' ch'; ?></span>
-          </li>
-        </div>
+<!-- Fin du Carousel -->
+    
+    <div>
+        <h1 class="carTitle"><?=$carDetails['modele']; ?></h1>
         <br>
-        <div class="guarantie">
-          <h2>Guarantie :</h2>
-          <span class=""><?= $carDetails['guarantie'].' mois'; ?></span>
-        </div>
-      </div>
-      <div class="carInfos">
-        <div class="carInterieur">
-          <li class="carInterieur">
-            <h2>Intérieur :</h2>
-<?php $dataInterieur= explode(',', $carDetails['interieur']);
-      foreach ($dataInterieur as $value)
-  {?>
-        <span class="interieur"><?= $value.'<br>'; ?></span>
-<?php } ?>
-          </li>
-        </ul>
-      </div>
-      <br>
-      <div class="chassis">
-        <h2>Chassis :</h2>
-  <?php $dataChassis= explode(',', $carDetails['chassis']);
-      foreach ($dataChassis as $value)
-      {?>
-        <span class=""><?= $value.'<br>'; ?></span>
-<?php } ?>
-      <div class="autre">
-<?php if (isset ($carDetails['autre'])&& !empty($carDetails['autre'])){?>
-        <h2>Autre :</h2><br>
-<?php
-      $dataAutre= explode(',', $carDetails['autre']);
-      foreach ($dataAutre as $value){?>
-        <span class="autre"><?= $value.'<br>'; ?></span>
-    <?php }}; ?>   
-      </div>
+        <h1 class="carContent"><?= $carDetails['carContent']; ?></h1>  
     </div>
-  </div>
+    <div class="logo-card">
+        <div class="logos">
+            <img src="../assets/logo/logo_carburant.svg">
+            <p class=""><?= $carDetails['energy'];?></p>
+        </div>
+    <div class="logos">
+        <img src="../assets/logo/logo_compteur_vitesse.svg">
+        <p class=""><?= $carDetails['km']; ?></p>
+    </div>
+    <div class="logos">
+        <img src="../assets/logo/logo_calendrier.svg">
+        <p class=""><?= $carDetails['year']; ?></p>
+    </div>
+</div>
+<hr>
+<div class="carCaracteristique">
+    <div class="carGeneral">
+        <ul>  
+            <div class="carDetail">
+            <h2>Caracteristiques générales :</h2>
+                <li>                
+                    <span class="titleDescritpion">Couleur :</span>
+                    <span class="color"><?= $carDetails['color']; ?></span>
+                </li>
+                <li>
+                    <span class="titleDescritpion">Boite :</span>
+                    <span class="carBoite"><?= $carDetails['carBoite']; ?></span>
+                </li>
+                <li>
+                    <span class="titleDescritpion">Nbre de porte :</span>
+                    <span class="carDoor"><?= $carDetails['carDoor']; ?></span>
+                </li>
+                <li>
+                    <span class="titleDescritpion">PuissanceFiscale :</span>
+                    <span class="puissanceFiscale"><?= $carDetails['puissanceFiscale'].' CV'; ?></span>
+                </li>
+                <li>
+                    <span class="titleDescritpion">Puissance :(DIN)</span>
+                    <span class="Puissance"><?= $carDetails['Puissance'].' ch'; ?></span>
+                </li>
+            </div>
+            <br>
+    <div class="guarantie">
+        <h2>Guarantie :</h2>
+        <span class=""><?= $carDetails['guarantie'].' mois'; ?></span>
+    </div>
+</div>
+<div class="carInfos">
+    <div class="carInterieur">
+        <li class="carInterieur">
+        <h2>Intérieur :</h2>
+        <?php $dataInterieur= explode(',', $carDetails['interieur']);
+        foreach ($dataInterieur as $value)
+        {?>
+        <span class="interieur"><?= $value.'<br>'; ?></span>
+        <?php } ?>
+        </li>
+    </ul>
+    </div>
+    <br>
+    <div class="chassis">
+        <h2>Chassis :</h2>
+        <?php $dataChassis= explode(',', $carDetails['chassis']);
+        foreach ($dataChassis as $value)
+        {?>
+        <span class=""><?= $value.'<br>'; ?></span>
+        <?php } ?>
+    <div class="autre">
+        <?php if (isset ($carDetails['autre'])&& !empty($carDetails['autre'])){?>
+        <h2>Autre :</h2>
+        <br>
+        <?php $dataAutre= explode(',', $carDetails['autre']);
+        foreach ($dataAutre as $value){?>
+        <span class="autre"><?= $value.'<br>'; ?></span>
+        <?php }}; ?>   
+    </div>
+    </div>
+</div>
 </div>
     <hr>
       <div class="prix">
@@ -163,21 +154,21 @@ if (!empty($dossiersImages)) {
         <h2 class="prix"><?= $carDetails['price']." "."€"; ?></h2>  
       </div>
     <hr>
-  <div class="footerOccasion">  
+<div class="footerOccasion">  
     <div style="text-align: center;"><?= "Référence de l'annonce : 00".$carDetails['id']; ?></div>
-      <div class=""> 
-        <div>
-        <button class="demandeInfo  button" id="demandeInfo" name="demandeInfo">Demande d'info</button>
+        <div class=""> 
+            <div>
+                <button class="demandeInfo  button" id="demandeInfo" name="demandeInfo">Demande d'info</button>
+            </div>
+            <div> 
+                <button  class="back button" onclick="window.location.href = '../templates/nos_occasions.php';">Retour</button>
+            </div>
         </div>
-        <div> 
-        <button  class="back button" onclick="window.location.href = '../templates/nos_occasions.php';">Retour</button>
-        </div>
-        
-      </div>
     </div>
   <?php ?>
-  </div>
-<?php ?>
+</div>
+<?php } 
+}?>
 </div>
 </form>
 <?php include '../templates/footer.php' ?>
--- test/file.php
+++ test/file.php
@@ -1,66 +1,171 @@
 <?php 
-
 include('../config/sessionStop.php');
 include('../Session/variable.php');
 include('../config/configsql.php');
 include('../templates/header.php');?>
 
-<!--<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sélectionner et afficher une image</title>
-</head>
-<body>
-    <h2>Sélectionner une image</h2>
-    <form action="../uploads/upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="fileInput" id="fileInput" accept="image/*" onchange="previewImage()">
-        <br><br>
-        <img id="imagePreview" src="#" alt="Aperçu de l'image" style="max-width: 300px; max-height: 300px;">
-        <br><br>
-        <input type="submit" value="Télécharger et Afficher">
-    </form>
-    //page de modification de services-editservice.php-->
-<?php $services = getservice($adminpdo);
+<?php 
+if (isset($_GET['id'])) {
+    $carId = $_GET['id'];
+// Utiliser l'ID pour récupérer les détails de l'annonce depuis la base de données
+    $carDetails = getCarById($adminpdo,$carId);}
+  // Vérifier si l'annonce existe
+    if ($carDetails) {
+//recuperation des images de l'ID
+    $sql = "SELECT img1, img2, img3, img4 FROM cars WHERE id = :id  ";
+    $statement = $adminpdo->prepare($sql);  
+   $statement->bindParam(':id',$carId);
+    $statement->execute();
+    $images = $statement->fetchAll(PDO::FETCH_ASSOC);
+// Parcourir le tableau d'images
+    foreach ($images as $image) {
+        $img1 = $image['img1'];
+        $img2 = $image['img2'];
+        $img3 = $image['img3'];
+        $img3 = $image['img4'];?>
 
-foreach ($services as $service){?>  
-        <form action="../Session/editservice.php" enctype= "multipart/form-data" method="POST">
-            <h2>Modification de services N°: <?php echo $service['id'] ?></h2> 
-            <input type="hidden" value="<?= $service['id']; ?>" name="id"/>
-            <div class="formulaire">
-                <div class="form-group">
-                    <label for="fileInput">Choisir un fichier :</label>
-                    <input type="file" name="fileInput" id="fileInput" accept="image/*" onchange="previewImage()">
-                    <br><br>
-                    <img id="imagePreview" src="#" alt="Aperçu de l'image" style="max-width: 300px; max-height: 300px;">
-                </div>
-                <div>
-                    <label for="imgBdd">Image actuelle :</label>
-                    <img id="imgBdd"  name="imgBdd" style="max-width: 300px; max-height: 300px;" src="<?php echo ($service['servicesPicture']);?>">
-                </div>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb p-3 bg-body-tertiary rounded-3">
+        <li class="breadcrumb-item"><a href="../templates/index.php">Accueil </a></li>
+        <li class="breadcrumb-item"><a href="../templates/nos_occasions.php">Nos occasions </a></li>
+        <li class="breadcrumb-item active" aria-current="page"></li>
+    </ol>
+</nav>
+<div class="container  announcement-container announcement-<?= $carDetails['id']?>" >  
+    <!-- Carousel -->
+    <div id="myCarousel" class="carousel slide mb-6 pointer-event" data-bs-ride="carousel">
+        <div class="carousel-inner text-center">
+            <div class="carousel-item <?= $index === 1 ? 'active' : '' ?>">
+                <img class="card-img imgcarousel" src="<?= $img1 ?>" alt="Image 1">
             </div>
-<?php } ?>
+            <div class="carousel-item ">
+                <img class="card-img imgcarousel" src="<?= $img2 ?>" alt="Image 1">
+            </div>
+            <div class="carousel-item ">
+                <img class="card-img imgcarousel" src="<?= $img3 ?>" alt="Image 1">
+            </div>
+            <div class="carousel-item ">
+                <img class="card-img imgcarousel" src="<?= $img4 ?>" alt="Image 1">
+            </div>
+                <!-- Ajouter les deux autres images de la même manière -->
+            <?php } ?>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+<!-- Fin du Carousel -->
+    
+    <div>
+        <h1 class="carTitle"><?=$carDetails['modele']; ?></h1>
+        <br>
+        <h1 class="carContent"><?= $carDetails['carContent']; ?></h1>  
+    </div>
+    <div class="logo-card">
+        <div class="logos">
+            <img src="../assets/logo/logo_carburant.svg">
+            <p class=""><?= $carDetails['energy'];?></p>
+        </div>
+    <div class="logos">
+        <img src="../assets/logo/logo_compteur_vitesse.svg">
+        <p class=""><?= $carDetails['km']; ?></p>
+    </div>
+    <div class="logos">
+        <img src="../assets/logo/logo_calendrier.svg">
+        <p class=""><?= $carDetails['year']; ?></p>
+    </div>
+</div>
+<hr>
+<div class="carCaracteristique">
+    <div class="carGeneral">
+        <ul>  
+            <div class="carDetail">
+            <h2>Caracteristiques générales :</h2>
+                <li>                
+                    <span class="titleDescritpion">Couleur :</span>
+                    <span class="color"><?= $carDetails['color']; ?></span>
+                </li>
+                <li>
+                    <span class="titleDescritpion">Boite :</span>
+                    <span class="carBoite"><?= $carDetails['carBoite']; ?></span>
+                </li>
+                <li>
+                    <span class="titleDescritpion">Nbre de porte :</span>
+                    <span class="carDoor"><?= $carDetails['carDoor']; ?></span>
+                </li>
+                <li>
+                    <span class="titleDescritpion">PuissanceFiscale :</span>
+                    <span class="puissanceFiscale"><?= $carDetails['puissanceFiscale'].' CV'; ?></span>
+                </li>
+                <li>
+                    <span class="titleDescritpion">Puissance :(DIN)</span>
+                    <span class="Puissance"><?= $carDetails['Puissance'].' ch'; ?></span>
+                </li>
+            </div>
+            <br>
+    <div class="guarantie">
+        <h2>Guarantie :</h2>
+        <span class=""><?= $carDetails['guarantie'].' mois'; ?></span>
+    </div>
+</div>
+<div class="carInfos">
+    <div class="carInterieur">
+        <li class="carInterieur">
+        <h2>Intérieur :</h2>
+        <?php $dataInterieur= explode(',', $carDetails['interieur']);
+        foreach ($dataInterieur as $value)
+        {?>
+        <span class="interieur"><?= $value.'<br>'; ?></span>
+        <?php } ?>
+        </li>
+    </ul>
+    </div>
+    <br>
+    <div class="chassis">
+        <h2>Chassis :</h2>
+        <?php $dataChassis= explode(',', $carDetails['chassis']);
+        foreach ($dataChassis as $value)
+        {?>
+        <span class=""><?= $value.'<br>'; ?></span>
+        <?php } ?>
+    <div class="autre">
+        <?php if (isset ($carDetails['autre'])&& !empty($carDetails['autre'])){?>
+        <h2>Autre :</h2>
+        <br>
+        <?php $dataAutre= explode(',', $carDetails['autre']);
+        foreach ($dataAutre as $value){?>
+        <span class="autre"><?= $value.'<br>'; ?></span>
+        <?php }}; ?>   
+    </div>
+    </div>
+</div>
+</div>
+    <hr>
+      <div class="prix">
+        <h2 class="prix">Prix :</h2>
+        <h2 class="prix"><?= $carDetails['price']." "."€"; ?></h2>  
+      </div>
+    <hr>
+<div class="footerOccasion">  
+    <div style="text-align: center;"><?= "Référence de l'annonce : 00".$carDetails['id']; ?></div>
+        <div class=""> 
+            <div>
+                <button class="demandeInfo  button" id="demandeInfo" name="demandeInfo">Demande d'info</button>
+            </div>
+            <div> 
+                <button  class="back button" onclick="window.location.href = '../templates/nos_occasions.php';">Retour</button>
+            </div>
+        </div>
+    </div>
+  <?php ?>
+</div>
+<?php }?>
+</div>
 </form>
-<script>
-    function previewImage() {
-        var fileInput = document.getElementById('fileInput');
-        var imagePreview = document.getElementById('imagePreview');
-
-        // Vérifie si un fichier a été sélectionné
-        if (fileInput.files && fileInput.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                // Affiche l'aperçu de l'image
-                imagePreview.src = e.target.result;
-            }
-
-            // Lit le contenu du fichier sélectionné
-            reader.readAsDataURL(fileInput.files[0]);
-        }
-    }
-</script>
-</body>
 <?php include '../templates/footer.php' ?>
-</html>
+<script>demandeInfo()</script>
